99c3c2646f9582d94b1917d9d3f6f3cbd77889c2
Ty <justfacey@gmail.com>
2024-01-29 13:26:31 -0500

    pass third stage

--- app/main.py
+++ app/main.py
@@ -1,23 +1,27 @@
+import os
 import subprocess
 import sys
+import time
+
 
 
 def main():
     command = sys.argv[3]
     args = sys.argv[4:]
     
+    # Execute commands with args
+    parent_process = subprocess.Popen([command, *args], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-
-    
-
-    completed_process = subprocess.Popen([command, *args], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-    stdout, stderr =  completed_process.communicate() # wait for process to end
+    # Wait for process to end
+    stdout, stderr =  parent_process.communicate()
 
     if stderr:
-        print(stderr.decode("utf-8"), file=sys.stderr, end='') # 'file' tells python to print output as stderr explicitly
+        print(stderr.decode("utf-8"), file=sys.stderr, end='') # Set 'file=' to tell python to print output as stderr
     if stdout:
         print(stdout.decode("utf-8"), end='')
+    
+    # Return exit stauts
+    sys.exit(parent_process.returncode)
 
 
 if __name__ == "__main__":
