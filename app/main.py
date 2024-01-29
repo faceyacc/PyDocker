f6e91c407da692c7901b9bc3fe6cf6d521a843bf
Ty <justfacey@gmail.com>
2024-01-29 01:34:47 -0500

    secure the bag :)

--- app/main.py
+++ app/main.py
@@ -3,16 +3,21 @@ import sys
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
-    # Uncomment this block to pass the first stage
-    #
     command = sys.argv[3]
     args = sys.argv[4:]
     
-    completed_process = subprocess.run([command, *args], capture_output=True)
-    print(completed_process.stdout.decode("utf-8"))
+
+
+    
+
+    completed_process = subprocess.Popen([command, *args], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    stdout, stderr =  completed_process.communicate() # wait for process to end
+
+    if stderr:
+        print(stderr.decode("utf-8"), file=sys.stderr, end='') # 'file' tells python to print output as stderr explicitly
+    if stdout:
+        print(stdout.decode("utf-8"), end='')
 
 
 if __name__ == "__main__":
