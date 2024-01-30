9d6c080cdf55ec9d75edb1acbae0a0b4055521e7
Ty <justfacey@gmail.com>
2024-01-29 23:36:15 -0500

    on & on

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
+#!/bin/python
+
 import os
+import shutil
 import subprocess
 import sys
+import tempfile
 import time
 
 
@@ -8,6 +12,18 @@ import time
 def main():
     command = sys.argv[3]
     args = sys.argv[4:]
+
+    # Create an empty temporary directory
+    tmp_path = tempfile.mkdtemp()
+
+    # copy executable into temp dir using shutil.copy2()
+    shutil.copy2(command, tmp_path)
+
+    # chroot into temp dir 
+    os.chroot(tmp_path)
+
+    command = os.path.join("/", os.path.basename(command))
+
     
     # Execute commands with args
     parent_process = subprocess.Popen([command, *args], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
