ac651268dd6f7fbde5bb3f1fef285c8fb1b8983b
Ty <justfacey@gmail.com>
2024-01-30 14:43:15 -0500

    isolate pid namespaces

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 #!/bin/python
 
+import ctypes
 import os
 import shutil
 import subprocess
@@ -16,7 +17,7 @@ def main():
     # Create an empty temporary directory
     tmp_path = tempfile.mkdtemp()
 
-    # copy executable into temp dir using shutil.copy2()
+    # Copy executable into temp dir using shutil.copy2()
     shutil.copy2(command, tmp_path)
 
     # chroot into temp dir 
@@ -24,6 +25,14 @@ def main():
 
     command = os.path.join("/", os.path.basename(command))
 
+
+    # Load in c types to use unshare 
+    libc = ctypes.cdll.LoadLibrary("libc.so.6")
+
+    # Pass in CLONE_NEWPID flag to unshare
+    # Unshare the PID namespace so parent a child processes have seperate namespaces
+    libc.unshare(0x20000000)
+    
     
     # Execute commands with args
     parent_process = subprocess.Popen([command, *args], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
