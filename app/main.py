a4036f7e428554a03d46fcbbc0b9c11f9b86e974
Ty <justfacey@gmail.com>
2024-02-02 21:17:58 -0500

    added helper functions to get Docker Hub token

--- app/main.py
+++ app/main.py
@@ -4,6 +4,26 @@ import sys
 import tempfile
 import os
 import ctypes
+import requests
+
+def get_token(image):
+    url = f"https://auth.docker.io/token?service=registry.docker.io&scope=repository:library/{image}:pull"
+    with requests(url) as response:
+        body = response.read()
+
+
+def get_manifest(image, token):
+    # set headers
+    distribution_type = "application/vnd.docker.distribution.manifest.list.v2+json"
+
+    headers = {
+        "authorization" : token,
+        "accept" : distribution_type,
+    }
+
+    url = f"https://registry-1.docker.io/v2/library/{image}/latest"
+
+    # return requests.request
 
 
 def main():
@@ -20,17 +40,18 @@ def main():
     # make temp dir root
     os.chroot(tmp_dir)
 
+    command = os.path.join("/", os.path.basename(command))
+
     libc = ctypes.LibraryLoader("libc.so.6")
 
     # Pass in CLONE_NEWPID flag to unshare
     # Unshare the PID namespace so parent a child processes have seperate namespaces
     libc.unshare(0x20000000)
+    
+    
+    process = subprocess.Popen([command, *args], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr =  process.communicate()
 
-    command = os.path.join("/", os.path.basename(command))
-    
-    
-    process = subprocess.Popen([command, *args], stderr=subprocess.PIPE, stdout=subprocess.PIPE)
-    stderr, stdout = process.communicate()
 
     if stderr:
         print(stderr.decode("utf-8"), file=sys.stderr, end="")
