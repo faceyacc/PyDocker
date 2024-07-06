27270291f18eb91019eabbfb482ffb5277702665
faceyacc <justfacey@gmail.com>
2024-07-06 19:36:37 -0400

    added some documentation to code.

--- app/main.py
+++ app/main.py
@@ -9,8 +9,17 @@ import urllib.request
 import json
 
 
+def get_token(image: str):
+    """
+    Get token from Docker auth server by making GET req using image from args
 
-def get_token(image):
+    Args:
+        image (str): image name
+
+    Returns:
+        str: Docker auth token
+    """
+
     url = f"https://auth.docker.io/token?service=registry.docker.io&scope=repository:library/{image}:pull"
     res = urllib.request.urlopen(url)
 
@@ -18,13 +27,24 @@ def get_token(image):
 
     return res_json["token"]
 
-    
+
 def get_manifest(image, token):
+
+    """
+    Get image manifest for specified image (from 'command') from Docker Hub
+
+    Args:
+        image (str): image name
+        token (str): Docker auth token
+
+    Returns:
+        list: layers Docker Hub's registry
+    """
 
     url = f"https://registry-1.docker.io/v2/library/{image}/manifests/latest"
 
     request = urllib.request.Request(
-            url,     
+            url,
             headers={
                     "Accept" : "application/vnd.docker.distribution.manifest.v2+json",
                     "Authorization": "Bearer " + token,
@@ -36,16 +56,28 @@ def get_manifest(image, token):
 
     return res_json["layers"]
 
-    
-  
+
+
 def pull_layers(image, token, layers):
+    """
+    Download layers from manifest file and put result a tarfile (call it manifest.tar)
+
+    Args:
+        image (str): image name
+        token (str): Docker auth token
+        layers (list): layers Docker Hub's registry
+
+    Returns:
+        str: path to directory where layers are stored
+    """
+
     dir_path = tempfile.mkdtemp()
-    
+
     # 3. Download layers from manifest file and put result a tarfile (call it manifest.tar)
     for layer in layers:
         url = f"https://registry.hub.docker.com/v2/library/{image}/blobs/{layer['digest']}"
         request = urllib.request.Request(
-            url,     
+            url,
             headers={
                     "Accept" : "application/vnd.docker.distribution.manifest.v2+json",
                     "Authorization": "Bearer " + token,
@@ -60,16 +92,27 @@ def pull_layers(image, token, layers):
 
     os.remove(tmp_file)
     return dir_path
-        
+
 
 def run_command(command, args, dir_path):
-    # chroot into temp dir 
+    """
+    Execute commands with args
+
+    Args:
+        command (str): command to run
+        args (list): arguments for command
+        dir_path (str): path to directory where layers are stored
+
+    Returns:
+        None
+    """
+    # chroot into temp dir
     os.chroot(dir_path)
-    
+
     # command = os.path.join("/", os.path.basename(command))
 
 
-    # Load in c types to use unshare 
+    # Load in c types to use unshare
     libc = ctypes.cdll.LoadLibrary("libc.so.6")
 
     # Unshare the PID namespace so parent a child processes have seperate namespaces
@@ -77,16 +120,16 @@ def run_command(command, args, dir_path):
 
     # Execute commands with args
     parent_process = subprocess.Popen([command, *args], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    
+
 
     # Wait for process to end
     stdout, stderr =  parent_process.communicate()
-    
+
     if stderr:
         print(stderr.decode("utf-8"), file=sys.stderr, end='') # Set 'file=' to tell python to print output as stderr
     if stdout:
-        print(stdout.decode("utf-8"), end='')    
-    
+        print(stdout.decode("utf-8"), end='')
+
     # Return exit stauts
     sys.exit(parent_process.returncode)
 
@@ -102,7 +145,7 @@ def main():
 
     # 2. using the token from above get image manifest for specified image (from 'command') from Docker Hub
     layers = get_manifest(image=image, token=token)
-    
+
     # 3. Download layers from manifest file and put result a tarfile (call it manifest.tar)
     dir_path = pull_layers(image, token, layers)
 
